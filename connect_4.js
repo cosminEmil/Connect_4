7198534bf51e19675c4278ec3e0fbf4dee6709b6
cosminEmil <132258074+cosminEmil@users.noreply.github.com>
2023-08-04 14:50:31 +0300

    Update connect_4.js

--- connect_4.js
+++ connect_4.js
@@ -1,17 +1,18 @@
+let container = document.getElementById("container");
+let elements = container.getElementsByTagName("div");
 let players_turn = 0;
 
-document.addEventListener("DOMContentLoaded", function() {
-    let container = document.getElementById("buttons");
-    for (let i = 1; i <= 42; ++i) {
-        let button = document.createElement("button");
-        button.setAttribute("id", "btn" + i);
-        button.addEventListener("click", function() {
+function makeRows(rows, cols) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for (let i = 1; i <= rows * cols; ++i) {
+        let cell = document.createElement("div");
+        cell.innerHTML = i;
+        cell.addEventListener("click", function() {
             if (players_turn % 2 == 0) {
-                button.classList.add("btn", "btn-danger");
-                button.innerText = "red";
+                cell.style.backgroundColor = "red";
             } else if (players_turn % 2) {
-                button.classList.add("btn", "btn-warning");
-                button.innerText = "yellow";
+                cell.style.backgroundColor = "yellow";
             }
             ++players_turn;
             if (players_turn == 42) {
@@ -19,79 +20,39 @@ document.addEventListener("DOMContentLoaded", function() {
                 window.location.reload();
                 return 0;
             }
-            left(button, i);
-            right(button, i);
-            checkWinner(button, i);
         });
-        if (i <= 7) {
-            button.setAttribute("class", "row_1");
-        } else if (i > 7 && i <= 14) {
-            button.setAttribute("class", "row_2");
-        } else if (i > 14 && i <= 21) {
-            button.setAttribute("class", "row_3");
-        } else if (i > 21 && i <= 28) {
-            button.setAttribute("class", "row_4");
-        } else if (i > 28 && i <= 35) {
-            button.setAttribute("class", "row_5");
-        } else if (i > 35 && i <= 42) {
-            button.setAttribute("class", "row_6");
-        }
-        container.appendChild(button);
+        container.appendChild(cell).className = "grid-item";
     }
-});
-let container = document.getElementById("buttons");
-let buttons = container.getElementsByTagName("button");
-
-function checkColor(btnColor) {
-    if (btnColor == "red") {
-        alert("Player 1 wins");
-    } else if (btnColor == "yellow") {
-        alert("Player 2 wins");
-    }
-    window.location.reload();
-    return 0;
 }
 
-function checkWinner(button, btnIndex) {
-    for (let k = 0, adder = 5; k < 3; ++k, ++adder) {
-        let j = 0; i = btnIndex, winCnt = 0;
-        while (j < 3) {
-            if (button.innerText == buttons[i + adder].innerText) {
-                ++winCnt;
+makeRows(6, 7);
+
+function left(cellIndex) {
+
+}
+
+function leftDiagonal(cellIndex) {
+
+}
+
+function down(cellIndex) {
+    for (let i = 1; i <= 7; ++i) {
+        let redCounter = 0, yellowCounter = 0;
+        for (let j = i; j <= i + 35; j += 7) {
+            
+            if (elements[j].style.backgroundColor == 'red') {
+                ++redCounter;
+            } else if (elements[j].style.backgroundColor == 'yellow') {
+                ++yellowCounter;
             }
-            ++j;
-            i += adder + 1;
-        }
-        if (winCnt == 3) {
-            checkColor(button.innerText);
         }
     }
 }
 
-function left(button, btnIndex) {
-    let j = 0, i = btnIndex, winCnt = 0;
-    while (j < 3) {
-        if (button.innerText == buttons[i - 2].innerText) {
-            ++winCnt;
-        }
-        ++j;
-        --i;
-    }
-    if (winCnt == 3) {
-        checkColor(button.innerText);
-    }
+function rightDiagonal(cellIndex) {
+
 }
 
-function right(button, btnIndex) {
-    let j = 0, i = btnIndex, winCnt = 0;
-    while (j < 3) {
-        if (button.innerText == buttons[i].innerText) {
-            ++winCnt;
-        }
-        ++j;
-        ++i;
-    }
-    if (winCnt == 3) {
-        checkColor(button.innerText);
-    }
+function right(cellIndex) {
+
 }
